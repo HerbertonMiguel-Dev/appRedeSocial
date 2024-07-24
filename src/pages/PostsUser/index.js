5ab08364e48b51073c44d4c4a8abc2babd3ece6a
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-07-24 17:22:10 -0300

    foram criadas as pastas peges as routes

---
+++ src/pages/PostsUser/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function PostsUser(){
+  return(
+    <View>
+      <Text>TELA PostsUser</Text>
+    </View>
+  )
+}
+
+export default PostsUser;
