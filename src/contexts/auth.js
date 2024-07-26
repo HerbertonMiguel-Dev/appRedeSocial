8408d8462ef853facc466e2c57e4f30290056279
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-07-26 11:34:18 -0300

    feito autenticação no login

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -8,15 +8,16 @@ import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-
-
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
 
   const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
   
   async function signUp(email, password, name){
+    setLoadingAuth(true);
+
     await auth().createUserWithEmailAndPassword(email, password)
     .then( async (value) =>{
       let uid = value.user.uid
@@ -33,17 +34,43 @@ function AuthProvider({ children }){
         }
 
         setUser(data);
-
+        setLoadingAuth(false);
       })
     })
     .catch(() =>{
-      alert('')
+      alert('usuario não cadastrado')
+      setLoadingAuth(false);
+    })
+  }
+
+  async function signIn(email, password){
+    setLoadingAuth(true);
+    await auth().signInWithEmailAndPassword(email, password)
+    .then(async(value) =>{
+      let uid = value.user.uid;
+
+      const userProfile = await firestore().collection('users')
+      .doc(uid).get();
+
+      let data = {
+        uid: uid,
+        nome: userProfile.data().nome,
+        email: value.user.email
+      }
+
+      setUser(data)
+      setLoadingAuth(false);
+
+    })
+    .catch(() =>{
+      alert('login falhou')
+      setLoadingAuth(false);
     })
   }
 
 
   return(
-    <AuthContext.Provider value={{ signed: !! user, signUp }}>
+    <AuthContext.Provider value={{ signed: !! user, signUp, signIn, loadingAuth }}>
       {children}
     </AuthContext.Provider>
   )
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,128 +1,146 @@
-import React, {useState, useContext} from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useContext } from "react";
+import { View, Text, ActivityIndicator } from "react-native";
 
-import { Container, Title, Input, Button, ButtonText, SignUpButton, SignUpText, TitleSenac } from './styles'
+import {
+  Container,
+  Title,
+  Input,
+  Button,
+  ButtonText,
+  SignUpButton,
+  SignUpText,
+} from "./styles";
 
-import {AuthContext} from '../../contexts/auth'
+import { AuthContext } from "../../contexts/auth";
 
-function Login(){
-  const [login, setLogin] = useState(true)
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+function Login() {
+  const [login, setLogin] = useState(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const { signUp } = useContext(AuthContext) 
+  const { signUp, signIn, loadingAuth } = useContext(AuthContext);
 
-
- function toggleLogin(){
-  setLogin(!login)
-  setName('')
-  setEmail('')
-  setPassword('')
- }
-
- function handleSignIn(){
-    if(email === '' && password === ''){
-        alert('Por favor, preencha os campos Email e senha');
-        return
-    }if (email === '') {
-        alert('Por favor, preencha o campo de email.');
-        return;
-    }
-
-    if (password === '') {
-        alert('Por favor, preencha o campo de senha.');
-        return;
-    }
+  function toggleLogin() {
+    setLogin(!login);
+    setName("");
+    setEmail("");
+    setPassword("");
   }
 
-  async function handleSignUp(){
-    if(name === '' && email === '' && password === ''){
-        alert('Por favor, preencha os campos Nome, Email e Senha');
-        return
-    }if (name === '') {
-        alert('Por favor, preencha o campo de Nome.');
-        return;
+  async function handleSignIn() {
+    if (email === "" && password === "") {
+      alert("Por favor, preencha os campos Email e senha");
+      return;
+    }
+    if (email === "") {
+      alert("Por favor, preencha o campo de email.");
+      return;
     }
 
-    if (email === '') {
-        alert('Por favor, preencha o campo de Email.');
-        return;
-    }
-    if (password === '') {
-        alert('Por favor, preencha o campo de Senha.');
-        return;
+    if (password === "") {
+      alert("Por favor, preencha o campo de senha.");
+      return;
     }
 
-    await signUp(email, password, name)
+    await signIn(email, password);
   }
 
+  async function handleSignUp() {
+    if (name === "" && email === "" && password === "") {
+      alert("Por favor, preencha os campos Nome, Email e Senha");
+      return;
+    }
+    if (name === "") {
+      alert("Por favor, preencha o campo de Nome.");
+      return;
+    }
 
+    if (email === "") {
+      alert("Por favor, preencha o campo de Email.");
+      return;
+    }
+    if (password === "") {
+      alert("Por favor, preencha o campo de Senha.");
+      return;
+    }
 
-  if(login){
-      return(
+    await signUp(email, password, name);
+  }
+
+  if (login) {
+    return (
       <Container>
         <Title>
-          Dev<Text style={{color: '#F7921C'}}>Post</Text><Text style={{color: '#004B8C', fontSize: 75}}> Senac</Text>
-          </Title>
-        <Input 
+          Dev<Text style={{ color: "#F7921C" }}>Post</Text>
+          <Text style={{ color: "#004B8C", fontSize: 75 }}> Senac</Text>
+        </Title>
+        <Input
           placeholder="Seu email"
           value={email}
-          onChangeText={ (text)  => setEmail(text) }
+          onChangeText={(text) => setEmail(text)}
         />
 
-        <Input 
+        <Input
           placeholder="*********"
           value={password}
-          onChangeText={ (text) => setPassword(text)}
+          onChangeText={(text) => setPassword(text)}
           secureTextEntry={true}
         />
-
         <Button onPress={handleSignIn}>
-          <ButtonText>Acessar</ButtonText>
+          {loadingAuth ? (
+            <ActivityIndicator size={20} color="#fff" />
+          ) : (
+            <ButtonText>Acessar</ButtonText>
+          )}
         </Button>
 
         <SignUpButton onPress={toggleLogin}>
           <SignUpText>Criar uma conta</SignUpText>
         </SignUpButton>
       </Container>
-    )
+    );
   }
 
-  return(
+  return (
     <Container>
-       <Title>
-          Dev<Text style={{color: '#F7921C'}}>Post</Text><Text style={{color: '#004B8C', fontSize: 75}}> Senac</Text>
-          </Title>
+      <Title>
+        Dev<Text style={{ color: "#F7921C" }}>Post</Text>
+        <Text style={{ color: "#004B8C", fontSize: 75 }}> Senac</Text>
+      </Title>
 
-      <Input 
+      <Input
         placeholder="Seu Seu nome"
         value={name}
-        onChangeText={ (text)  => setName(text) }
+        onChangeText={(text) => setName(text)}
       />
 
-      <Input 
+      <Input
         placeholder="Seu email"
         value={email}
-        onChangeText={ (text)  => setEmail(text) }
+        onChangeText={(text) => setEmail(text)}
       />
 
-      <Input 
+      <Input
         placeholder="*********"
         value={password}
-        onChangeText={ (text) => setPassword(text)}
+        onChangeText={(text) => setPassword(text)}
         secureTextEntry={true}
       />
 
       <Button onPress={handleSignUp}>
-        <ButtonText>Cadastrar</ButtonText>
+        {loadingAuth ? (
+          <ActivityIndicator size={20} color="#fff" />
+        ) : (
+          <ButtonText>Cadastrar</ButtonText>
+        )}
       </Button>
 
       <SignUpButton onPress={toggleLogin}>
         <SignUpText>Já Tenho uma conta</SignUpText>
       </SignUpButton>
     </Container>
-  )
+  );
 }
 
 export default Login;
