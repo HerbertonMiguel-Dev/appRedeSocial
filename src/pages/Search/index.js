01da584dc550b4903e8436f331d9390e638a1a8b
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-02 10:05:32 -0300

    implementando tela Search

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text } from 'react-native'
 
+import Feather from 'react-native-vector-icons/Feather'
+
+import { Container, AreaInput, Input, List } from './styles'
+
 function Search(){
+  const [input, setInput] = useState('')
+
   return(
-    <View>
-      <Text>TELA Search</Text>
-    </View>
+    <Container>
+      <AreaInput>
+        <Feather 
+          name='search'
+          size={20} 
+          color="#004B8C"
+        />
+        <Input 
+          placeholder="Procurando Usuario?"
+          value={input}
+          onChangeText={ (text) => setInput(text)}
+          placeholderTextColor="#353840"
+
+        />
+
+      </AreaInput>
+    </Container>
   )
 }
 
